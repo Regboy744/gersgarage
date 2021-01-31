fdce386298a3be06e6831dcfbbd17c8617436b32
Regboy744 <carvalho.ie18@gmail.com>
2021-01-31 16:09:37 +0000

    first commit

--- Api/Validation/stockParts.js
+++ Api/Validation/stockParts.js
@@ -2,18 +2,18 @@ const Joi = require("@hapi/joi");
 
 //REGISTER VALIDATION
 const stockPartsValidation = (data) => {
-  const schema = {
-    factory_ref: Joi.string().min(2).required(),
-    category: Joi.string().min(5).required(),
-    make: Joi.string().min(4).required(),
-    model: Joi.string().min(3).required(),
-    variante: Joi.string().min(2).required(),
-    engine: Joi.string().min(5).required(),
-    name: Joi.string().min(3).required(),
-    description: Joi.string().min(10).required(),
-    price: Joi.number().min(1).required(),
-    quantity: Joi.number().min(1).required(),
-  };
-  return Joi.validate(data, schema);
+     const schema = {
+          factory_ref: Joi.string().min(2).required(),
+          category: Joi.string().min(5).required(),
+          make: Joi.string().min(4).required(),
+          model: Joi.string().min(3).required(),
+          variante: Joi.string().min(2).required(),
+          engine: Joi.string().min(5).required(),
+          name: Joi.string().min(3).required(),
+          description: Joi.string().min(10).required(),
+          price: Joi.string().min(1).required(),
+          quantity: Joi.string().min(1).required(),
+     };
+     return Joi.validate(data, schema);
 };
 module.exports.stockPartsValidation = stockPartsValidation;
